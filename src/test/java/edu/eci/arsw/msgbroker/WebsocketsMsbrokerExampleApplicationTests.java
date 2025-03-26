24e5e30c142996d0c58981543e72307d48b5cb38
Manuel Suarez <manuel.suarez.e09@gmail.com>
2025-03-26 09:43:42 -0500

    PARTE FINAL

--- src/test/java/edu/eci/arsw/msgbroker/WebsocketsMsbrokerExampleApplicationTests.java
+++ src/test/java/edu/eci/arsw/msgbroker/WebsocketsMsbrokerExampleApplicationTests.java
@@ -6,11 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = edu.eci.arsw.collabpaint.CollabPaintApplication.class)
 public class WebsocketsMsbrokerExampleApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
-
 }
